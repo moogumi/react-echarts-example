280fed39d438ac68067f9defb3b51784bb1aab75
Eduard Kryshtapovich <e.krishtapovich@gmail.com>
2019-10-04 10:42:59 +0300

    charts api changes

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import HumanList from './components/humans/HumanList';
-import PopulationChart from './components/charts/PopulationChart';
+import Statistics from './components/charts/Statistics';
 import AddHuman from './components/humans/AddHuman';
 import HumanSummary from './components/humans/HumanSummary';
 import Footer from './components/layout/Footer';
@@ -19,7 +19,11 @@ class App extends Component {
             <Route exact path="/" component = { HumanList }></Route>                        
             <Route path="/add" component = { AddHuman }></Route>                        
             <Route path="/human/:id" component = { HumanSummary }></Route>
-            <Route path="/chart" component = { PopulationChart }></Route>
+
+            <Route path='/chart' render={(props) => (
+              <Statistics {...props} chartType={ "HumanAgeGroups" }/>
+            )}/>            
+
             <Route path="/error" component = { UnknownError }></Route>
             <Route path="*" component={ _404 }/>
           </Switch>          
